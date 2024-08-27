d2fec437c2ce42881dbc8078d3ad4b759a4e77a0
Janaka dev <janakchamantha12@gmail.com>
2024-08-27 08:50:23 +0530

    other:step 1

--- Program.cs
+++ Program.cs
@@ -12,8 +12,22 @@ namespace ConsoleApp1
         {
             Console.WriteLine("hello world");
             int age = 56;
+            int num = 6000000;
+            Console.WriteLine(int.MaxValue);
             Console.WriteLine(age);
+            Console.WriteLine(num);
+
+
+            string name = "janaka";
+            char initial = 'k';
+            Console.WriteLine(name);
+            Console.Write(initial);
+            Console.Write(initial);
+
             Console.ReadLine();
+
+
+
         }
     }
 }
