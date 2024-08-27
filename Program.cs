3dabaead0dc7210b8a03f835fe4ef69dad1289a9
Janaka dev <janakchamantha12@gmail.com>
2024-08-27 10:36:41 +0530

    other:step3 - simple if else

--- Program.cs
+++ Program.cs
@@ -10,15 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            const int num1 = 10;
-            const int num2 = 15;
-            int remainder;
-            remainder = num1 % num2;
-            Console.WriteLine(remainder);
+            Console.Write("enter your anme:");
+            string name=Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("enter your age:");
+
+            string input_age =Console.ReadLine();
+
+            int age=Convert.ToInt32(input_age);
+
+            Console.WriteLine("your name is : "+name+" and your age is:"+input_age);
+            if (age>20)
+            {
+                Console.WriteLine("your are too old");
+            }else if (age>14)
+            {
+                Console.WriteLine("your are a teen");
+            }
+
             Console.ReadLine();
-
-
-
         }
     }
 }
