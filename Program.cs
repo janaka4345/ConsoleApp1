e3798e0bb9b050daa65c6099733d13bbd66a39c5
Janaka dev <janakchamantha12@gmail.com>
2024-08-27 06:14:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            int age = 56;
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
